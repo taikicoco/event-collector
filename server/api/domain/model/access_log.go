e675eef720fe4825b3faf7601506bd01a91b7b70
taikicoco <kawasakitaiki0308@gmail.com>
2023-06-02 21:48:18 +0900

    chore:go fmt

--- server/api/domain/model/access_log.go
+++ server/api/domain/model/access_log.go
@@ -5,10 +5,10 @@ import (
 )
 
 type AccessLog struct {
-	ID        uint      `json:"id" gorm:"primaryKey"`
-	AccessLogDetailID uint `json:"access_log_detail_id"`
-	Access   uint      `json:"access"`
-	Conversion uint     `json:"conversion"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
+	ID                uint      `json:"id" gorm:"primaryKey"`
+	AccessLogDetailID uint      `json:"access_log_detail_id"`
+	Access            uint      `json:"access"`
+	Conversion        uint      `json:"conversion"`
+	CreatedAt         time.Time `json:"createdAt"`
+	UpdatedAt         time.Time `json:"updatedAt"`
 }
--- server/api/domain/model/access_log_detail.go
+++ server/api/domain/model/access_log_detail.go
@@ -6,8 +6,8 @@ import (
 
 type AccessLogDetail struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
-	Name	  string    `json:"name"`
-	Version   uint    `json:"version"`
+	Name      string    `json:"name"`
+	Version   uint      `json:"version"`
 	PageUrl   string    `json:"page_url"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`

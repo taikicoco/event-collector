a8e9f1e3ad20cda6b7dbcdacd3aae5d3da61c2ce
taikicoco <kawasakitaiki0308@gmail.com>
2023-06-04 17:25:06 +0900

    update:handler

--- server/api/handler/handler.go
+++ server/api/handler/handler.go
@@ -10,42 +10,81 @@ import (
 )
 
 func AssignRoutes(e *echo.Echo) error {
-	e.GET("/log", getData)
-	e.POST("/log", createData)
+	e.POST("/log/detail", createLogDetail)
+	e.GET("/log/detail/:id", getLogDetail)
+	e.POST("/log/:id", createLog)
 	return nil
 }
 
-type Log struct {
-	Log uint `json:"log"`
+type createLogDetailRequest struct {
+	Name    string `json:"name"`
+	Version uint   `json:"version"`
+	PageUrl string `json:"page_url"`
 }
 
-func getData(e echo.Context) error {
+type getLogDetailRequest struct {
+	ID uint `json:"id" param:"id"`
+}
+
+type createLogRequest struct {
+	LogDetailID uint `json:"id" param:"id"`
+	Access      uint `json:"access"`
+	Conversion  uint `json:"conversion"`
+}
+
+func createLogDetail(e echo.Context) error {
+	var r createLogDetailRequest
+	if err := e.Bind(&r); err != nil {
+		return err
+	}
+
+	logDetail := model.LogDetail{
+		Name:    r.Name,
+		Version: r.Version,
+		PageUrl: r.PageUrl,
+	}
 
 	db := dbInit()
-
-	var log model.Log
-
-	l := db.First(&log, 1)
-
+	l := db.Create(&logDetail)
 	err := l.Error
 	if err != nil {
 		return err
 	}
-	return e.JSON(http.StatusOK, log)
-	// return e.JSON(http.StatusOK, "data")
+	return e.JSON(http.StatusOK, logDetail)
 }
 
-func createData(e echo.Context) error {
-	log := new(Log)
-	log.Log = 1
+func getLogDetail(e echo.Context) error {
 
-	if err := e.Bind(log); err != nil {
+	var r getLogDetailRequest
+	if err := e.Bind(&r); err != nil {
 		return err
 	}
 
 	db := dbInit()
 
-	l := db.Create(log)
+	var logDetail model.LogDetail
+	l := db.First(&logDetail, r.ID)
+	err := l.Error
+	if err != nil {
+		return err
+	}
+	return e.JSON(http.StatusOK, logDetail)
+}
+
+func createLog(e echo.Context) error {
+	var rl createLogRequest
+	if err := e.Bind(&rl); err != nil {
+		return err
+	}
+
+	log := model.Log{
+		LogDetailID: rl.LogDetailID,
+		Access:      rl.Access,
+		Conversion:  rl.Conversion,
+	}
+
+	db := dbInit()
+	l := db.Create(&log)
 	err := l.Error
 	if err != nil {
 		return err

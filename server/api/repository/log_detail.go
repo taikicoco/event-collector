0a6dcba7d729fc27392797e0c0714402602474ba
taikicoco <kawasakitaiki0308@gmail.com>
2023-06-07 20:39:57 +0900

    add:repository layer

---
+++ server/api/repository/log_detail.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"context"
+	"server/api/domain/model"
+)
+
+type LogDetailRepository interface {
+	Create(context.Context, *model.LogDetail) (*model.LogDetail, error)
+	Get(context.Context, uint) (*model.LogDetail, error)
+}
+
+type logDetailRepository struct{}
+
+func NewLogDetailRepository() LogDetailRepository {
+	return &logDetailRepository{}
+}
+
+func(ld *logDetailRepository) Create(ctx context.Context, logDetail *model.LogDetail) (*model.LogDetail, error) {
+	db := dbInit()
+	l := db.Create(logDetail)
+	err := l.Error
+	if err != nil {
+		return nil, err
+	}
+	return logDetail, nil
+}
+
+func(ld *logDetailRepository) Get(ctx context.Context, id uint) (*model.LogDetail, error) {
+	db := dbInit()
+
+	var logDetail *model.LogDetail
+	l := db.First(&logDetail, id)
+	err := l.Error
+	if err != nil {
+		return nil, err
+	}
+	return logDetail, nil
+}
